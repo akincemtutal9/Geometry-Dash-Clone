8c88190b6e51138b2a5400dae6ab2bc75de6c62d
Akın Cem <79974860+akincemtutal9@users.noreply.github.com>
2023-08-11 16:36:14 +0300

    feat: upside down gravity

--- Assets/GameAssets/Scripts/Player/PlayerController.cs
+++ Assets/GameAssets/Scripts/Player/PlayerController.cs
@@ -11,7 +11,7 @@ namespace GameAssets.Scripts.Player
         [SerializeField] private GameModes currentGameMode;
         
         [Header("References")]
-        [SerializeField] private float[] speedValues = {8.6f, 12.96f, 19.27f};
+        [SerializeField] private float[] speedValues = {};
         [SerializeField] private Transform groundCheckTransform;
         [SerializeField] private LayerMask groundMask;
         [SerializeField] private Transform playerSprite;
@@ -22,14 +22,17 @@ namespace GameAssets.Scripts.Player
         [SerializeField] private float groundCheckRadius;
         
         private Rigidbody2D rb;
+        private int _gravity = 1;
         
         private void Start()
         {
             rb = GetComponent<Rigidbody2D>();
             HandleMovement();
-            HandleJumpInput();
+            LimitFallSpeed();
+            HandleGameModeBehaviour();
         }
 
+        
         private void HandleMovement()
         {
             Observable.EveryUpdate()
@@ -38,6 +41,35 @@ namespace GameAssets.Scripts.Player
                     var moveSpeedIndex = (int)currentMoveSpeed;
                     transform.position += Vector3.right * speedValues[moveSpeedIndex] * Time.deltaTime;
                 }).AddTo(this);
+        }
+        private void HandleGameModeBehaviour()
+        {
+            Observable.EveryUpdate().Subscribe(_ =>
+            {
+                Invoke(currentGameMode.ToString(), 0);
+            }).AddTo(gameObject);
+        }
+        private void Cube()
+        {
+            Observable.EveryFixedUpdate()
+                .Subscribe(_ =>
+                {
+                    if (OnGrounded())
+                    {
+                        var rotation = playerSprite.rotation.eulerAngles;
+                        rotation.z = Mathf.Round(rotation.z / 90) * 90;
+                        playerSprite.rotation = Quaternion.Euler(rotation); // Complete the rotation
+                        if (Input.GetMouseButton(0))
+                        {
+                            Jump();
+                        }
+                    }
+                    else
+                    {
+                        playerSprite.Rotate(Vector3.back ,_gravity * rotationSpeed * Time.deltaTime);
+                    }
+                })
+                .AddTo(this);
         }
         private void HandleJumpInput()
         {
@@ -56,7 +88,7 @@ namespace GameAssets.Scripts.Player
                     }
                     else
                     {
-                        playerSprite.Rotate(Vector3.back * rotationSpeed);
+                        playerSprite.Rotate(Vector3.back , rotationSpeed * _gravity * Time.deltaTime );
                     }
                 })
                 .AddTo(this);
@@ -64,11 +96,27 @@ namespace GameAssets.Scripts.Player
         private void Jump()
         {
             rb.velocity = Vector2.zero;
-            rb.AddForce(Vector2.up * jumpForce , ForceMode2D.Impulse);
+            rb.AddForce(Vector2.up * jumpForce * _gravity, ForceMode2D.Impulse);
         }
         private bool OnGrounded()
         {
-            return Physics2D.OverlapCircle(groundCheckTransform.position, groundCheckRadius, groundMask);
+            return Physics2D.OverlapBox(groundCheckTransform.position+Vector3.up- Vector3.up*(_gravity -1 / -2), Vector2.right * 1.1f + Vector2.up * groundCheckRadius,0,groundMask);
+        }
+        private bool TouhcWall()
+        {
+            return Physics2D.OverlapBox((Vector2)transform.position + (Vector2.right * 0.55f),
+                Vector2.up * 0.8f + (Vector2.right * groundCheckRadius), 0, groundMask);
+        }
+
+        private void LimitFallSpeed()
+        {
+            Observable.EveryFixedUpdate().Subscribe(_ =>
+            {
+                if (rb.velocity.y < -24f)
+                {
+                    rb.velocity = new Vector2(rb.velocity.x, -24f);
+                }
+            }).AddTo(gameObject);
         }
 
         public void ChangeThroughPortal(MoveSpeed moveSpeed, GameModes gameMode , Gravity gravity, int state)
@@ -82,6 +130,7 @@ namespace GameAssets.Scripts.Player
                     currentGameMode = gameMode;
                     break;
                 case 2:
+                    _gravity = (int)gravity;
                     rb.gravityScale = Mathf.Abs(rb.gravityScale) * (int)gravity;
                     break;
             }
