0a7bbf6f4ad64dffd47d12a87c88b5d55090480d
Akın Cem <79974860+akincemtutal9@users.noreply.github.com>
2023-08-11 19:42:39 +0300

    feat: Ship Movement & fix: OnGrounded

--- Assets/GameAssets/Scripts/Player/PlayerController.cs
+++ Assets/GameAssets/Scripts/Player/PlayerController.cs
@@ -6,24 +6,22 @@ namespace GameAssets.Scripts.Player
 {
     public class PlayerController : MonoBehaviour
     {
-        [Header("Enum")]
-        [SerializeField] private MoveSpeed currentMoveSpeed;
+        [Header("Enum")] [SerializeField] private MoveSpeed currentMoveSpeed;
         [SerializeField] private GameModes currentGameMode;
-        
-        [Header("References")]
-        [SerializeField] private float[] speedValues = {};
-        [SerializeField] private Transform groundCheckTransform;
+
+        [Header("References")] 
         [SerializeField] private LayerMask groundMask;
         [SerializeField] private Transform playerSprite;
-        
-        [Header("Settings")]
+
+        [Header("Settings")] 
+        [SerializeField] private float[] speedValues = { };
         [SerializeField] private float jumpForce = 10f;
         [SerializeField] private float rotationSpeed = 5f;
         [SerializeField] private float groundCheckRadius;
-        
+
         private Rigidbody2D rb;
         private int _gravity = 1;
-        
+
         private void Start()
         {
             rb = GetComponent<Rigidbody2D>();
@@ -32,7 +30,6 @@ namespace GameAssets.Scripts.Player
             HandleGameModeBehaviour();
         }
 
-        
         private void HandleMovement()
         {
             Observable.EveryUpdate()
@@ -42,57 +39,42 @@ namespace GameAssets.Scripts.Player
                     transform.position += Vector3.right * speedValues[moveSpeedIndex] * Time.deltaTime;
                 }).AddTo(this);
         }
+
         private void HandleGameModeBehaviour()
         {
-            Observable.EveryUpdate().Subscribe(_ =>
-            {
-                Invoke(currentGameMode.ToString(), 0);
-            }).AddTo(gameObject);
+            Observable.EveryUpdate().Subscribe(_ => { Invoke(currentGameMode.ToString(), 0); }).AddTo(gameObject);
         }
+
         private void Cube()
         {
-            Observable.EveryFixedUpdate()
-                .Subscribe(_ =>
+            if (OnGrounded())
+            {
+                var rotation = playerSprite.rotation.eulerAngles;
+                rotation.z = Mathf.Round(rotation.z / 90) * 90;
+                playerSprite.rotation = Quaternion.Euler(rotation); // Complete the rotation
+                if (Input.GetMouseButton(0))
                 {
-                    if (OnGrounded())
-                    {
-                        var rotation = playerSprite.rotation.eulerAngles;
-                        rotation.z = Mathf.Round(rotation.z / 90) * 90;
-                        playerSprite.rotation = Quaternion.Euler(rotation); // Complete the rotation
-                        if (Input.GetMouseButton(0))
-                        {
-                            Jump();
-                        }
-                    }
-                    else
-                    {
-                        playerSprite.Rotate(Vector3.back ,_gravity * rotationSpeed * Time.deltaTime);
-                    }
-                })
-                .AddTo(this);
+                    Jump();
+                }
+            }
+            else
+            {
+                playerSprite.Rotate(Vector3.back, _gravity * rotationSpeed * Time.deltaTime);
+            }
         }
-        private void HandleJumpInput()
-        {
-            Observable.EveryFixedUpdate()
-                .Subscribe(_ =>
-                {
-                    if (OnGrounded())
-                    {
-                        var rotation = playerSprite.rotation.eulerAngles;
-                        rotation.z = Mathf.Round(rotation.z / 90) * 90;
-                        playerSprite.rotation = Quaternion.Euler(rotation); // Complete the rotation
-                        if (Input.GetMouseButton(0))
-                        {
-                            Jump();
-                        }
-                    }
-                    else
-                    {
-                        playerSprite.Rotate(Vector3.back , rotationSpeed * _gravity * Time.deltaTime );
-                    }
-                })
-                .AddTo(this);
-        }
+    
+
+        private void Ship()
+        { 
+            transform.rotation = Quaternion.Euler(0, 0, rb.velocity.y * 2);
+            
+            if (Input.GetMouseButton(0))
+                rb.gravityScale = -4.314969f;
+            else 
+                rb.gravityScale = 4.314969f;
+            rb.gravityScale *= _gravity;
+            }
+        
         private void Jump()
         {
             rb.velocity = Vector2.zero;
@@ -100,7 +82,7 @@ namespace GameAssets.Scripts.Player
         }
         private bool OnGrounded()
         {
-            return Physics2D.OverlapBox(groundCheckTransform.position+Vector3.up- Vector3.up*(_gravity -1 / -2), Vector2.right * 1.1f + Vector2.up * groundCheckRadius,0,groundMask);
+            return Physics2D.OverlapBox(transform.position + Vector3.down * _gravity * 0.5f, Vector2.right * 1.1f + Vector2.up * groundCheckRadius, 0, groundMask);
         }
         private bool TouhcWall()
         {
