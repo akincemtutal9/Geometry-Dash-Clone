72be5b33211f42de843a6ddac86d703ba19032fa
Akın Cem <79974860+akincemtutal9@users.noreply.github.com>
2023-08-11 13:10:03 +0300

    fix: bad ground check & feat: move
    removed DoTween Rotation

--- Assets/GameAssets/Scripts/Player/PlayerController.cs
+++ Assets/GameAssets/Scripts/Player/PlayerController.cs
@@ -1,63 +1,51 @@
-using DG.Tweening;
 using UniRx;
-using UniRx.Triggers;
 using UnityEngine;
 
 namespace GameAssets.Scripts.Player
 {
+    public enum MoveSpeed{Slow = 0 , Fast = 1 , VeryFast = 2};
     public class PlayerController : MonoBehaviour
     {
-        private const string Ground = nameof(Ground);
-        
+        private readonly float[] speedValues = {8.6f,12.96f,19,27f};
+        [SerializeField] private MoveSpeed currentMoveSpeed;
         [SerializeField] private float jumpForce = 10f;
-        [SerializeField] private float rotationDuration = 1f;
-
+        [SerializeField] private Transform groundCheckTransform;
+        [SerializeField] private float groundCheckRadius;
+        [SerializeField] private LayerMask groundMask;
+        
         private Rigidbody2D rb;
         
-        private bool isGrounded = true;
-        private bool rotateClockwise = true;
-
         private void Start()
         {
             rb = GetComponent<Rigidbody2D>();
+            HandleMovement();
             HandleJumpInput();
-            HandleGroundCollisionEnter();
-            HandleGroundCollisionExit();
+        }
+
+        private void HandleMovement()
+        {
+            Observable.EveryUpdate()
+                .Subscribe(_ =>
+                {
+                    var moveSpeedIndex = (int)currentMoveSpeed;
+                    transform.position += Vector3.right * speedValues[moveSpeedIndex] * Time.deltaTime;
+                }).AddTo(this);
         }
         private void HandleJumpInput()
         {
             Observable.EveryFixedUpdate()
-                .Where(_ => Input.GetKeyDown(KeyCode.Space) && isGrounded) 
-                .Subscribe(_ => JumpAndRotateObject())
+                .Where(_ => Input.GetMouseButton(0) && OnGrounded()) 
+                .Subscribe(_ => Jump())
                 .AddTo(this);
         }
-        private void JumpAndRotateObject()
+        private void Jump()
         {
-            RotateObject();
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            rb.velocity = Vector2.zero;
+            rb.AddForce(Vector2.up * jumpForce , ForceMode2D.Impulse);
         }
-        private void RotateObject()
+        private bool OnGrounded()
         {
-            float rotationAngle = rotateClockwise ? 180f : -180f;
-            transform.DORotate(new Vector3(0, 0, transform.eulerAngles.z + rotationAngle), rotationDuration)
-                .OnComplete(() =>
-                {
-                    rotateClockwise = !rotateClockwise;
-                });
-        }
-        private void HandleGroundCollisionEnter()
-        {
-            this.OnCollisionEnter2DAsObservable()
-                .Where(collision => collision.gameObject.CompareTag(Ground))
-                .Subscribe(_ => isGrounded = true)
-                .AddTo(this);
-        }
-        private void HandleGroundCollisionExit()
-        {
-            this.OnCollisionExit2DAsObservable()
-                .Where(collision => collision.gameObject.CompareTag(Ground))
-                .Subscribe(_ => isGrounded = false)
-                .AddTo(this);
+            return Physics2D.OverlapCircle(groundCheckTransform.position, groundCheckRadius, groundMask);
         }
     }
 }
